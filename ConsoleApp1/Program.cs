a0ab0308a5ac5bfc51f4993c537ca5bafa0a4ea0
Emil <emilv.kjellin@gmail.com>
2023-11-28 09:51:21 +0100

    har fått fram någon ide av hur mitt spel ska se ut

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿Console.WriteLine("Feed man");
+int weight = 500;
 
+
+
+while( weight <=500 && weight >= 400)
+{
+Console.WriteLine("You need to lose weight!");
+weight -= 2;
+Console.ReadKey();
+Console.ReadLine();
+
+}
+Console.ReadLine();
+if( weight <= 400 && weight <=300)
+{
+Console.WriteLine("Good work now you can either build muscle or continue loosing weight!");
+
+}
