b098d5097e07e48037c7287f3b8eab3a2575cbb9
Emil <emilv.kjellin@gmail.com>
2024-01-11 23:20:29 +0100

    Har lagt till ett object i konstant rörelse, timer, ett sätt att klara spelet, finslipat hela spelet generellt.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,12 +12,15 @@ int x = 400;
 int y = 300;
 int rectanglex = 1150;
 int rectangley = 310;
+double timer = 0;
+int velocityx = 5;
+int menutext = 30;
 rectanglex += 3;
-
 Texture2D Batman = Raylib.LoadTexture("BATMAN2.png");
 Texture2D jumpscare = Raylib.LoadTexture("baba.png");
-Texture2D menu = Raylib.LoadTexture("OIP.png");
-
+Texture2D menu = Raylib.LoadTexture("batman meny.png");
+Texture2D menubild = Raylib.LoadTexture("Batmandope.png");
+Texture2D joker = Raylib.LoadTexture("Joker.png");
 Sound sound = Raylib.LoadSound("yt1s.com_-_last_christmas__sped_up[1].wav");
 Sound menusound = Raylib.LoadSound("yt1s.com_-_Earrape_Scream__Sound_Effect[1].wav");
 
@@ -31,6 +34,8 @@ menu.Width = 1920;
  int jumpscare1 = 0;
 
  int game = 0;
+
+
 
 
 while (!Raylib.WindowShouldClose())
@@ -130,24 +135,51 @@ if ( x > 1100 && x < 1900 && y > 100 && y <280)
  jumpscare1 = 1;
 }
 
+if ( x > 1800 && x < 2000 && y > 100 && y <900)
+{
+ jumpscare1 = 1;
+}
+
+if (x > 200 && x < 250 && y > 800 && y < 900)
+{
+game = 2;
+x = 250;
+y = 850;
+}
+if (x > 0 && x < 300 && y > 100 && y < 580 )
+{
+    jumpscare1 = 1;
+}
 /*Rörelser*/
-if (rectanglex > 1149 && rectanglex < 1900)
+
+rectanglex += velocityx;
+if (rectanglex >= 1800)
 {
-rectanglex += 3;
+    velocityx *= -1;
+
 }
-if (rectanglex == 1800)
+if ( rectanglex <= 1150)
 {
-    
-    
+velocityx += 1;
 }
+
+/* Timer */
+
+if (game == 1)
+{
+    timer += 0.01;
+}
+
 Raylib.BeginDrawing();
 
 
+
+
     Raylib.ClearBackground(Color.PURPLE);
-
-
+    
+    Raylib.DrawTexture(joker, 200, 800, Color.WHITE);
     Raylib.DrawTexture(Batman, x-25, y, Color.WHITE);
-
+    Raylib.DrawText($"Time: {timer}  ",25, 45, 50, Color.WHITE);
     if ( jumpscare1 == 1)
     {
         Raylib.DrawTexture(jumpscare, 0, 0, Color.WHITE);
@@ -155,8 +187,11 @@ Raylib.BeginDrawing();
         Raylib.DrawText("Press Space to return to menu ", 200, 740, 100,Color.WHITE);
     }
 
+
     if ( jumpscare1 == 0)
        {
+        Raylib.DrawRectangle( 300, 0, 350, 200, Color.BLACK);
+        Raylib.DrawRectangle( 0, 100, 300, 480, Color.BLACK);
         Raylib.DrawRectangle( 300, 380, 400, 200, Color.BLACK);
         Raylib.DrawRectangle(700, 180, 200, 400, Color.BLACK);
         Raylib.DrawRectangle(250, 100, 400, 200, Color.BLACK);
@@ -164,29 +199,65 @@ Raylib.BeginDrawing();
         Raylib.DrawRectangle(950, 0, 200, 400, Color.BLACK);
         Raylib.DrawRectangle(900, 450, 800, 200, Color.BLACK);
         Raylib.DrawRectangle(1100, 100, 800, 200, Color.BLACK);
+        Raylib.DrawRectangle(1800, 100, 200, 800, Color.BLACK);
 
         /*Objekt i rörelse*/
         
         Raylib.DrawRectangle(rectanglex, rectangley, 20, 50, Color.BLACK);
        }
 
-Raylib.EndDrawing();
+
 
 
 }
 if (game == 0)
 {
-    Raylib.BeginDrawing();
-       
-        Raylib.DrawTexture(menu, 0, 0, Color.WHITE);
-        Raylib.DrawText("Press Enter to start ", 400, 540, 100,Color.WHITE);
-    Raylib.EndDrawing();
+    Raylib.DrawTexture(menu, 0, 0, Color.WHITE);
+    Raylib.DrawText("start", 900, 500, menutext,Color.WHITE);
+    Raylib.DrawTexture(menubild , 650, -200, Color.WHITE);
+    
+ 
+    if (Raylib.GetMouseX() > 840 && Raylib.GetMouseX() < 1040 && Raylib.GetMouseY() > 450 && Raylib.GetMouseY() < 510)
+    {
+        
+        
 
-    if (Raylib.IsKeyDown(KeyboardKey.KEY_ENTER))
- {
-  game = 1;
- }
+        if (Raylib.IsMouseButtonDown(MouseButton.MOUSE_LEFT_BUTTON))
+        {
+            game = 1;
+            x = 400;
+            y = 300;
+        
+        }
+            
+    }
+      
 }
+if (game == 2)
+{
+    Raylib.ClearBackground(Color.BLACK);
+   
+    Raylib.DrawText("BATMAN CAPTURED JOKER!", 200, 540, 100, Color.WHITE);
+    Raylib.DrawText("Enter (restart)",1650,1000,30,Color.WHITE);
+    Raylib.DrawText("space (menu)",1400,1000,30,Color.WHITE);
+     if (Raylib.IsKeyDown(KeyboardKey.KEY_ENTER))
+     {
+        game = 1;
+        x = 400;
+        y = 300;
+     }
+        if (Raylib.IsKeyDown(KeyboardKey.KEY_SPACE))
+        {
+         game = 0;
+         x = 400;
+         y = 300;
+        }
+}
+
+
+Raylib.EndDrawing();
+
+
 /*Ljud*/
 
 if (jumpscare1 == 1)
@@ -197,6 +268,7 @@ if (game == 0)
 {
     Raylib.PlaySound(menusound);
 }
+
 }
 //unload
 /*
